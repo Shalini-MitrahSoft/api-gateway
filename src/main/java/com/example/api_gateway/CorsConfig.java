61dab94249a318452a40f348a4180a30ef991e48
thiyagumitrahsoft-tech <thiyagumitrahsoft@gmail.com>
2026-08-18 18:58:47 +0530

    Updated CorsErrorSolution

--- src/main/java/com/example/api_gateway/CorsConfig.java
+++ src/main/java/com/example/api_gateway/CorsConfig.java
@@ -14,7 +14,17 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "http://192.168.1.116:5173"
+        ));
+
+//        config.setAllowedOriginPatterns(List.of(
+//                "http://localhost:*"
+//                "http://127.0.0.1:*",
+//                "http://192.168.1.118:*"
+//        ));
+
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
